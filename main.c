1233859b97642c9bc0980d76f107dfdc97417c8a
barbara-aper <barbaraaper@gmail.com>
2024-07-15 16:49:03 -0300

    codificacao dos caracteres para bitmap funcional, vetor usando indice da tabela ascii

--- main.c
+++ main.c
@@ -66,50 +66,6 @@ int main(int argc, char *argv[])
 
     criaTabela(tabela, bm, a2);
 
-    int i;
-
-    printf("\nB ");
-    for (i = 0; i < bitmapGetLength(tabela['b']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela['b'], i));
-    }
-    printf("\n");
-
-    printf("M ");
-    for (i = 0; i < bitmapGetLength(tabela['m']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela['m'], i));
-    }
-    printf("\n");
-
-    printf("O ");
-    for (i = 0; i < bitmapGetLength(tabela['o']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela['o'], i));
-    }
-    printf("\n");
-
-    printf("E ");
-    for (i = 0; i < bitmapGetLength(tabela['e']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela['e'], i));
-    }
-    printf("\n");
-
-    printf("S ");
-    for (i = 0; i < bitmapGetLength(tabela['s']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela['s'], i));
-    }
-    printf("\n");
-
-    printf("ESPAÇO ");
-    for (i = 0; i < bitmapGetLength(tabela[' ']); i++)
-    {
-        printf("%d", bitmapGetBit(tabela[' '], i));
-    }
-    printf("\n");
-
     return 0;
 }
 
